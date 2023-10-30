e5866ed037cc5560869129064934bff55ba8958b
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-10-30 22:57:04 +0900

    Update About.js

--- src/About.js
+++ src/About.js
@@ -4,13 +4,14 @@ import "./About.css";
 const About = () => {
   const [activeSection, setActiveSection] = useState("history");
   const [imageIndex, setImageIndex] = useState(0);
-  const [images, setImages] = useState(
-    {../img/1.jpg},
+  //const [images, setImages] = useState(
+  const images=[
+    "../img/1.jpg",
     "../img/2.jpg",
     "../img/3.jpg",
     "../img/4.jpg",
     "../img/5.jpg",
-  );
+  ];
 
   const sections = {
     history: {

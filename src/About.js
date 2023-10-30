743b32f0132a65dd0886db27973d7d51f1da7ee6
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-10-30 22:54:50 +0900

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
   const [activeSection, setActiveSection] = useState("history");
   const [imageIndex, setImageIndex] = useState(0);
   const [images, setImages] = useState(
-    "../img/1.jpg",
+    {../img/1.jpg},
     "../img/2.jpg",
     "../img/3.jpg",
     "../img/4.jpg",

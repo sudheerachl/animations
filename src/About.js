1abd098baa530ea27cbdd601c1a8592e639ca794
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-10-30 22:58:39 +0900

    Update About.js

--- src/About.js
+++ src/About.js
@@ -6,11 +6,11 @@ const About = () => {
   const [imageIndex, setImageIndex] = useState(0);
   //const [images, setImages] = useState(
   const images=[
-    "../img/1.jpg",
-    "../img/2.jpg",
-    "../img/3.jpg",
-    "../img/4.jpg",
-    "../img/5.jpg",
+    "./img/1.jpg",
+    "./img/2.jpg",
+    "./img/3.jpg",
+    "./img/4.jpg",
+    "./img/5.jpg",
   ];
 
   const sections = {

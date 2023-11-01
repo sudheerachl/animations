48bc292e8ddcfc5d22cb20ea70349539681aaa42
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-01 22:31:04 +0900

    Update About.js

--- src/About.js
+++ src/About.js
@@ -10,11 +10,7 @@ const About = () => {
   const [imageIndex, setImageIndex] = useState(0);
   //const [images, setImages] = useState(
   const images=[
-   Image1,Image2,Image3,Image4,Image5,// "./img/1.jpg",
-   // "./img/2.jpg",
-    //"./img/3.jpg",
-    //"./img/4.jpg",
-    //"./img/5.jpg",
+   Image1,Image2,Image3,Image4,Image5,
   ];
 
   const sections = {
@@ -28,7 +24,59 @@ const About = () => {
     },
     accomplishments: {
       title : "Homoeopathy Myths and Facts: Busted!",
-      text: null,
+      const Address = () => {
+  const heading = "Homoeopathy Myths and Facts: Busted!";
+
+  const mythsAndFacts = [
+    {
+      myth: "Homoeopathy is just a placebo effect.",
+      fact: "There is growing scientific evidence to support the effectiveness of homeopathy.",
+      link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3960496/",
+    },
+    {
+      myth: "Homoeopathy is not safe.",
+      fact: "Homoeopathic remedies are made from highly diluted substances, which makes them very safe.",
+      link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4377390/",
+    },
+    {
+      myth: "Homoeopathy is not regulated.",
+      fact: "Homoeopathy is regulated by the government in many countries, including India.",
+      link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4377390/",
+    },
+    {
+      myth: "Homoeopathy is not effective for acute conditions.",
+      fact: "Homoeopathy can be effective for a wide range of acute conditions, such as colds, flu, and diarrhea. ",
+link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2930910/",
+    },
+    {
+      myth: "Homoeopathy is not effective for chronic conditions.",
+      fact: "Homoeopathy can be effective for a wide range of chronic conditions, including asthma, allergies, and eczema.",
+      link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2930908/",
+    },
+  ];
+
+  const handleButtonClick = (link) => {
+    window.open(link, "_blank");
+  };
+
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <ul>
+        {mythsAndFacts.map((mythAndFact) => (
+          <li key={mythAndFact.myth} className="myth-and-fact-item">
+            <h2 className="myth">{mythAndFact.myth}</h2>
+            <p className="text-black font-normal fact">{mythAndFact.fact}</p>
+            <button onClick={() => handleButtonClick(mythAndFact.link)}>
+              Read More
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
     },
   };
   let imageScrollTimer = null;

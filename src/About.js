f8f0719b8e13f9e41d653cc0acb07a63fd8c2f6c
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-01 22:45:51 +0900

    Update About.js

--- src/About.js
+++ src/About.js
@@ -24,7 +24,9 @@ const About = () => {
     },
     accomplishments: {
       title : "Homoeopathy Myths and Facts: Busted!",
-      text : 12333,
+      text : 'myth: "Homoeopathy is just a placebo effect.",
+      fact: "There is growing scientific evidence to support the effectiveness of homeopathy.",
+      link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3960496/"',
     },
   };
   let imageScrollTimer = null;

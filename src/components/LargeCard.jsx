c77fbfd80488de122c1b3969f96f421e26f4b134
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2024-02-06 22:51:03 +0530

    Update LargeCard.jsx

--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { FcVoicePresentation, FcInspection,FcApproval,FcDataProtection } from "react-icons/fc";
+import {
+ FcVoicePresentation,
+ FcInspection,
+ FcApproval,
+ FcDataProtection,
+} from "react-icons/fc";
 import "./largecard.css";
+
 const LargeCard = (props) => {
-  const loadcontent = props.load;
-  
-  return (
-   
-      {/* Right  */}
-      <div className="flex gap-10">
-       <p id="about" >
-          <Link to="/about" onClick={loadcontent}>About Us</Link>
-        </p>
-        
-        </div>
-  );
+ const { loadContent } = props; // Access prop using destructuring
+
+ return (
+   <div className="flex gap-10">
+     {/* Right */}
+     <p id="about">
+       <Link to="/about" onClick={loadContent}>About Us</Link>
+     </p>
+   </div>
+ );
 };
 
 export default LargeCard;
